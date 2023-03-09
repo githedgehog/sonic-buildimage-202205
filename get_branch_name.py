4c6357a9a1f1d773d0a843e3912ad2c41df67179
Maksym Hedeon <maksym@githedgehog.com>
2023-03-09 20:18:24 +0200

    refactor

--- get_branch_name.py
+++ get_branch_name.py
@@ -6,8 +6,8 @@ import subprocess
 def get_branch_by_hash():
     try:
         branch = os.environ.get('branch', "HEAD")
-        commit = os.environ.get('commit_id')
-
+        # commit = os.environ.get('commit_id')
+        commit = "5a75cc8b05e73e44a0adb70250f3aef17e864b4b"
         # Skip. No reason to find branch if repo is already on it
         if branch != "HEAD":
             print(branch)
@@ -16,17 +16,13 @@ def get_branch_by_hash():
 
         # bytes -> str -> list
         contains_lst = contains.decode("utf-8").split('\n')
-
-        # clean elements (remove empty, remove trailing)
         contains_lst.remove("")
-        for idx, _ in enumerate(contains_lst):
-            contains_lst[idx] = contains_lst[idx].replace("*", "").strip()
 
         # Branch can be defined by commit just in case if "git branch --contains"
         # returns 2 strings: 1st is "(HEAD detached at {hash})" and 2nd is a branch name.
         # In other cases branch cannot be defined so return HEAD
         if len(contains_lst) == 2 and "(HEAD detached at " in contains_lst[0]:
-            branch = contains_lst[1]
+            branch = contains_lst[1].strip()
 
         # print except return is to catch output by a bash script
         print(branch)
