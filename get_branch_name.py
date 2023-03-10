0b236eeac5e9ac018989b3a094b2dfa55dd0d163
Maksym Hedeon <maksym@githedgehog.com>
2023-03-10 20:02:54 +0200

    rework using git branch -a
    Signed-off-by: Maksym Hedeon <maksym@githedgehog.com>

--- get_branch_name.py
+++ get_branch_name.py
@@ -6,23 +6,25 @@ import subprocess
 def get_branch_by_hash():
     try:
         branch = os.environ.get('branch', "HEAD")
-        # commit = os.environ.get('commit_id')
-        commit = "5a75cc8b05e73e44a0adb70250f3aef17e864b4b"
+        commit = os.environ.get('commit_id')
+
         # Skip. No reason to find branch if repo is already on it
         if branch != "HEAD":
             print(branch)
 
-        contains = subprocess.check_output(['git', 'branch', '--contains', commit])
+        contains = subprocess.check_output(['git', 'branch', '-a', '--contains', commit])
 
         # bytes -> str -> list
         contains_lst = contains.decode("utf-8").split('\n')
-        contains_lst.remove("")
+        # get pure branch names
+        contains_lst = [elem.replace("remotes/origin/", "").replace("*", "").strip() for \
+                        elem in contains_lst if (elem != "") and ("HEAD" not in elem)]
 
-        # Branch can be defined by commit just in case if "git branch --contains"
-        # returns 2 strings: 1st is "(HEAD detached at {hash})" and 2nd is a branch name.
-        # In other cases branch cannot be defined so return HEAD
-        if len(contains_lst) == 2 and "(HEAD detached at " in contains_lst[0]:
-            branch = contains_lst[1].strip()
+        # remove duplicates
+        contains_lst = list(dict.fromkeys(contains_lst))
+
+        if len(contains_lst) == 1:
+            branch = contains_lst[0]
 
         # print except return is to catch output by a bash script
         print(branch)
